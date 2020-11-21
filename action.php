cc508a84d3d0f00708249117a4aa99c6f62a02b9
Anna Dabrowska <dabrowska@cosmocode.de>
2020-11-21 20:31:08 +0100

    Code refactoring, preparing for new features

--- action.php
+++ action.php
@@ -1,24 +1,37 @@
 <?php
 
-if (!defined('DOKU_INC')) die();
-
 class action_plugin_drawio extends DokuWiki_Action_Plugin
 {
 
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param \Doku_Event_Handler $controller
+     */
     public function register(Doku_Event_Handler $controller)
     {
-        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'handle_started');
-        $controller->register_hook('MENU_ITEMS_ASSEMBLY', 'AFTER', $this, 'add_button');
+        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'jsinfoEditPermissions');
+        $controller->register_hook('MENU_ITEMS_ASSEMBLY', 'AFTER', $this, 'addCreateButton');
     }
 
-    public function handle_started(Doku_Event $event, $param)
+    /**
+     * Add some permissions data to JSINFO
+     *
+     * @param Doku_Event $event
+     */
+    public function jsinfoEditPermissions(Doku_Event $event)
     {
         global $JSINFO;
         $JSINFO['iseditor'] = auth_quickaclcheck('*') >= AUTH_UPLOAD;
         $JSINFO['sectok'] = getSecurityToken();
     }
 
-    public function add_button(Doku_Event $event, $param)
+    /**
+     * Add "create diagram" to page tools menu
+     *
+     * @param Doku_Event $event
+     */
+    public function addCreateButton(Doku_Event $event)
     {
         if ($event->data['view'] != 'page') return;
         array_splice($event->data['items'], -1, 0, [new dokuwiki\plugin\drawio\MenuItem()]);
--- script.js
+++ script.js
@@ -1,12 +1,100 @@
 jQuery( document ).ready( function() {
+
+    const serviceUrl = 'https://embed.diagrams.net/?embed=1&proto=json&spin=1';
+    const doctypeXML = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+
+    // check if name/id of new diagram is valid
+    function validId(id) {
+        return id.length > 0 && /^[\w][\w\.\-]*$/.test( id )
+    }
+
+    // return URL to fetch existing diagram from DW
+    function getLocalDiagramUrl(ns, id) {
+        return DOKU_BASE + 'lib/exe/ajax.php?call=mediaupload&ow=true&ns=' + ns + '&qqfile=' + id + '&sectok=' + JSINFO['sectok'];
+    }
+
+    // split full id into ns and id parts
+    function splitFullId(fullId) {
+        let id = fullId;
+        let ns = '';
+        const idParts = id.split(':');
+        if (idParts.length > 1) {
+            ns = idParts[0];
+            id = idParts.slice(1).join(':');
+        }
+         return {ns: ns, id: id};
+    }
+
+    /**
+     * Launch diagram editor's iframe
+     *
+     * @param fullId
+     */
+    const launchEditor = function(fullId) {
+        if (!jQuery('#drawio-frame')[0]) {
+
+            jQuery('body').append('<iframe id="drawio-frame" style="border: 0;position: fixed; top: 0; left: 0; right:0; bottom: 0; width:100%; height:100%; z-index: 9999;"></iframe>');
+
+            jQuery(window).on('message', {fullId: fullId}, handleServiceMessages);
+            jQuery('#drawio-frame').attr('src', serviceUrl);
+        }
+    };
+
+    /**
+     * Handle messages from diagramming service
+     *
+     * @param e
+     */
+    const handleServiceMessages = function( e ) {
+        // get diagram info passed to the function
+        const fullId = e.data.fullId;
+        const {ns, id} = splitFullId(fullId);
+
+        const msg = JSON.parse( e.originalEvent.data );
+        const drawio = jQuery( '#drawio-frame' )[0].contentWindow;
+        if( msg.event === 'init' ) {
+            jQuery.get(DOKU_BASE + 'lib/exe/fetch.php?media=' + fullId, function (data) {
+                drawio.postMessage(JSON.stringify({action: 'load', xml: data}), '*');
+            }, 'text');
+        } else if ( msg.event === 'save' ) {
+            drawio.postMessage(
+                JSON.stringify( {action: 'export', format: 'xmlsvg', spin: 'Speichern' } ),
+                '*'
+            );
+        } else if ( msg.event === 'export' ) {
+            if ( msg.format !== 'svg' ) {
+                alert( 'Nicht unterstützt!' );
+            } else {
+                const datastr = doctypeXML + '\n' +
+                    decodeURIComponent( atob( msg.data.split( ',' )[1] ).split( '' ).map( function( c ) {
+                        return '%' + ( '00' + c.charCodeAt( 0 ).toString( 16 ) ).slice( -2 );
+                    } ).join( '' ) );
+                jQuery.post( getLocalDiagramUrl(ns, id), datastr )
+                    .done( function() {
+                        jQuery( window ).off( 'message', handleServiceMessages );
+                        jQuery( '#drawio-frame' ).remove();
+                        setTimeout( function() {
+                            location.reload();
+                        }, 200 );
+                    } ).fail( function() {
+                    alert( 'Fehler beim Speichern' );
+                } );
+            }
+        } else if( msg.event === 'exit' ) {
+            jQuery( window ).off( 'message', handleServiceMessages );
+            jQuery( '#drawio-frame' ).remove();
+        }
+    };
+
+    // add diagram edit button to all SVGs included in wiki pages
     if( JSINFO['iseditor'] ) {
         jQuery( 'img, object' ).filter( '.media, .medialeft, .mediacenter, .mediaright' ).add( 'iframe.svgpureinsert' ).each( function() {
-            var current = jQuery( this );
-            var src = this.nodeName == 'OBJECT' ? current.attr( 'data' ) : current.attr( 'src' );
-            var extension = src.split( '.' ).pop().toLowerCase();
-            if( extension == 'svg' ) {
-                var editlink = '<br><button class="drawio-btn btn btn-default btn-xs" style="clear:both" data-id="' + src.split('media=')[1].split('&')[0] + '">Editieren</button>';
-                if( current.parent()[0].nodeName == 'A' ) {
+            const current = jQuery( this );
+            const src = this.nodeName === 'OBJECT' ? current.attr( 'data' ) : current.attr( 'src' );
+            const extension = src.split( '.' ).pop().toLowerCase();
+            if( extension === 'svg' ) {
+                const editlink = '<br><button class="drawio-btn btn btn-default btn-xs" style="clear:both" data-id="' + src.split('media=')[1].split('&')[0] + '">Editieren</button>';
+                if( current.parent()[0].nodeName === 'A' ) {
                     current.parent().after( editlink );
                 } else {
                     current.after( editlink );
@@ -15,70 +103,25 @@ jQuery( document ).ready( function() {
         } );
     }
 
-    jQuery( 'button.drawio-btn' ).on( 'click', function() {
-        var drawio_url = 'https://embed.diagrams.net/?embed=1&proto=json&spin=1';
+    // attach diagram editing function to the button rendered on pages
+    jQuery( 'button.drawio-btn' ).on( 'click', function () {
+        const fullId = jQuery( this ).data( 'id' );
+        launchEditor(fullId);
+    });
 
-        if( !jQuery( '#drawio-frame' )[0] ) {
-            var fullId = jQuery( this ).data( 'id' );
-            var id = fullId;
-            var ns = '';
-            var ext = id.split( '.' ).pop().toLowerCase();
-            var idParts = id.split( ':' );
-            if( idParts.length > 1 ) {
-                ns = idParts[0];
-                id = idParts.slice( 1 ).join( ':' );
-            }
-            jQuery( 'body' ).append( '<iframe id="drawio-frame" style="border: 0;position: fixed; top: 0; left: 0; right:0; bottom: 0; width:100%; height:100%; z-index: 9999;"></iframe>' );
-            var onmessage = function( e ) {
-                var msg = JSON.parse( e.originalEvent.data );
-                var drawio = jQuery( '#drawio-frame' )[0].contentWindow;
-                if( msg.event == 'init' ) {
-                    jQuery.get( DOKU_BASE + 'lib/exe/fetch.php?media=' + fullId, function( data ) {
-                        drawio.postMessage( JSON.stringify( {action: 'load', xml: data} ), '*' );
-                    }, 'text' );
-                } else if( msg.event == 'save' ) {
-                    drawio.postMessage( JSON.stringify( {action: 'export', format: 'xmlsvg', spin: 'Speichern' } ), '*' );
-                } else if( msg.event == 'export' ) {
-                    if( msg.format != 'svg' ) {
-                        alert( 'Nicht unterstützt!' );
-                    } else {
-                        var datastr = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
-                            decodeURIComponent( atob( msg.data.split( ',' )[1] ).split( '' ).map( function( c ) {
-                                return '%' + ( '00' + c.charCodeAt( 0 ).toString( 16 ) ).slice( -2 );
-                            } ).join( '' ) );
-                        jQuery.post( DOKU_BASE + 'lib/exe/ajax.php?call=mediaupload&ow=true&ns=' + ns + '&qqfile=' + id + '&sectok=' + JSINFO['sectok'], datastr )
-                            .done( function() {
-                                jQuery( window ).off( 'message', onmessage );
-                                jQuery( '#drawio-frame' ).remove();
-                                setTimeout( function() {
-                                    location.reload();
-                                }, 200 );
-                            } ).fail( function() {
-                                alert( 'Fehler beim Speichern' );
-                            } );
-                    }
-                } else if( msg.event == 'exit' ) {
-                    jQuery( window ).off( 'message', onmessage );
-                    jQuery( '#drawio-frame' ).remove();
-                }
-            };
-            jQuery( window ).on( 'message', onmessage );
-            jQuery( '#drawio-frame' ).attr( 'src', drawio_url );
-        }
-    } );
-
+    // create a new diagram, triggered by click on page tools item
     jQuery( 'a#drawio-newfile-create' ).on( 'click', function( e ) {
         e.preventDefault();
-        var ns = NS;
-        var id = prompt( 'Name des neuen Diagramms' );
-        if( !/^[\w][\w\.\-]*$/.test( id ) ) {
-            alert( 'Dateiname enthält ungültige Zeichen' );
+        const ns = NS;
+        let id = prompt( 'Name des neuen Diagramms' );
+        if( !validId(id) ) {
+            alert( 'Dateiname ist leer oder enthält ungültige Zeichen' );
             return;
         }
         id += '.svg';
-        var datastr = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' +
+        const datastr = doctypeXML +
             '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1px" height="1px" version="1.1" content="&lt;mxfile userAgent=&quot;Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36&quot; version=&quot;7.9.5&quot; editor=&quot;www.draw.io&quot;&gt;&lt;diagram id=&quot;8c846276-93cf-00fc-3101-d1fabb6ae99a&quot; name=&quot;Seite-1&quot;&gt;dZHBEoIgEIafhrtCNXY2q0snD51JEJjQdRBH6+nTwIyxuLB8/7+7sCCSVsPJ0EZegHGNcMQGRA4I43iDd+M2kYcjSbJ3QBjFvGkBuXpyDyNPO8V4GxgtgLaqCWEBdc0LGzBqDPShrQQddm2o4CuQF1Sv6VUxK/0rttHCz1wJOXeOI6/caHEXBrra90OYlO/l5IrOtby/lZRB/4VIhkhqAKyLqiHleprtPDaXd/yjfu5teG1/JIzBUns8BB9Ishc=&lt;/diagram&gt;&lt;/mxfile&gt;" style="background-color: rgb(255, 255, 255);"><defs/><g transform="translate(0.5,0.5)"/></svg>';
-        jQuery.post( DOKU_BASE + 'lib/exe/ajax.php?call=mediaupload&ns=' + ns + '&qqfile=' + id + '&sectok=' + JSINFO['sectok'], datastr )
+        jQuery.post( getLocalDiagramUrl(ns, id), datastr )
             .done( function( response ) {
                 if( response.error ) {
                     alert( 'Fehler beim Speichern: ' + response.error );
