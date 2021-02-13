7c8e3cfcf9e4ac195796cf008b9e3dd048acd4bd
githubkoma <ignatius_tg@gmx.de>
2021-02-13 22:59:37 +0100

    Create settings.php

---
+++ lang/en/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * english language file for diagrams plugin
+ *
+ * @author Chris G. <githubkoma@weycloud.de>
+ */
+
+$lang['service_url']              = 'Defines which diagrams.net editor url is used.';
