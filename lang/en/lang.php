66ca1865def9fdb3b0a3cc54dbc31b267a6b4e51
Anna Dabrowska <dabrowska@cosmocode.de>
2022-04-07 12:59:41 +0200

    Catch insufficient permissions early
    If user has no permisssions to create a diagram in current namespace,
    remove the form altogether.

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,6 +5,7 @@ $lang['missingConfig'] = 'Missing configuration for SVG diagrams: Remember to ad
 $lang['js']['createButton'] = 'Create';
 $lang['js']['createLink'] = 'Create a diagram';
 $lang['js']['createIntro'] = 'Create a diagram in current namespace';
+$lang['js']['createForbidden'] = 'You do not have sufficient permissions';
 $lang['js']['editButton'] = 'Edit diagram';
 $lang['js']['errorInvalidId'] = 'Name is empty or contains invalid characters!';
 $lang['js']['errorSaving'] = 'Saving failed';
