a2dfd2d29138c8b694ce0433bc7deb6a7e3b091c
Minh <phandinhminh@protonmail.ch>
2023-01-24 22:50:23 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['missingConfig']         = 'Thiếu cấu hình cho sơ đồ SVG: Hãy nhớ thêm "hình ảnh svg/svg+xml" vào mime.local.conf';
+$lang['js']['createButton']    = 'Tạo';
+$lang['js']['createLink']      = 'Tạo một sơ đồ';
+$lang['js']['createIntro']     = 'Tạo một sơ đồ trong không gian tên namespace hiện tại';
+$lang['js']['createForbidden'] = 'Bạn không có đủ quyền hạn';
+$lang['js']['editButton']      = 'Chỉnh sửa sơ đồ';
+$lang['js']['errorInvalidId']  = 'Tên bị để trống hoặc có chứa các ký tự không hợp lệ!';
+$lang['js']['errorSaving']     = 'Lưu thất bại';
+$lang['js']['errorUnsupportedFormat'] = 'Định dạng file tệp tin không được hỗ trợ!';
+$lang['js']['saving']          = 'Đang lưu';
