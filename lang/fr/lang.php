13c982a9e50d68928bdf86213b589084fe367011
Schplurtz le Déboulonné <schplurtz@laposte.net>
2023-02-27 21:40:09 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['missingConfig']         = 'Configuration manquante pour les diagrammes SVG : Souvenez vous d\'ajouter "svg image/svg+xml" à conf/mime.local.conf';
+$lang['js']['createButton']    = 'Créer';
+$lang['js']['createLink']      = 'Créer un diagramme';
+$lang['js']['createIntro']     = 'Créer un diagramme dans la catégorie courante';
+$lang['js']['createForbidden'] = 'Vos permissions ne sont pas suffisantes';
+$lang['js']['editButton']      = 'Éditer le diagramme';
+$lang['js']['errorInvalidId']  = 'Le nom est vide ou contient des caractères invalides !';
+$lang['js']['errorSaving']     = 'Échec de l\'enregistrement';
+$lang['js']['errorUnsupportedFormat'] = 'Format de fichier non pris en charge !';
+$lang['js']['saving']          = 'Enregistre…';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['service_url']           = 'Définir quel URL d\'éditeur diagrams.net utiliser';
