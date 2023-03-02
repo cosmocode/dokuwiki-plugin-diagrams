a569557dcd8fe656fb6d970b33d49173ff2fe4b4
Anna Dabrowska <dabrowska@cosmocode.de>
2023-03-02 09:35:28 +0100

    Handle links in complex diagrams
    Some links are contained in divs with pointer-events="none", which makes
    them unclickable. This explicitly allows all pointer events for links in
    SVG diagrams.

--- script.js
+++ script.js
@@ -130,9 +130,8 @@ jQuery(function () {
 });
 
 // open links in diagrams in the browser window instead of SVG frame
-// TODO this will not work with DokuWiki master as of February 2021 (contentDocument is null)
 jQuery(window).on('load', function() {
     jQuery('object.diagrams-svg').each( function() {
-        jQuery(this.contentDocument).find('svg').find('a').attr('target', '_parent');
+        jQuery(this.contentDocument).find('svg').find('a').attr({'target': '_parent', 'style': 'pointer-events: all;'});
     });
 });
