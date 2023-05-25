5a651f4b90f042c9b80137d5f8c16c4c9d4fb218
Andreas Gohr <gohr@example.com>
2023-05-25 14:55:30 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,21 +1,31 @@
 <?php
 
-$lang['missingConfig'] = 'Fehlende Konfiguration für SVG Diagramme: Bitte "svg image/svg+xml" in mime.local.conf ergänzen';
-
-$lang['js']['createButton'] = 'Erstellen';
-$lang['js']['createLink'] = 'Diagramm erstellen';
-$lang['js']['createIntro'] = 'Diagramm im aktuellen Namensraum erstellen:';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Andreas Gohr <gohr@example.com>
+ */
+$lang['missingConfig']         = 'Fehlende Konfiguration für SVG Diagramme: Bitte "svg image/svg+xml" in mime.local.conf ergänzen';
+$lang['embedSaveSummary']      = 'Eingebettetes Diagramm aktualisiert';
+$lang['js']['createButton']    = 'Erstellen';
+$lang['js']['createLink']      = 'Diagramm erstellen';
+$lang['js']['createIntro']     = 'Diagramm im aktuellen Namensraum erstellen:';
 $lang['js']['createForbidden'] = 'Sie besitzen nicht die notwendigen Berechtigungen';
-$lang['js']['editButton'] = 'Diagramm editieren';
-$lang['js']['errorInvalidId'] = 'Name ist leer oder enthält ungültige Zeichen!';
-$lang['js']['errorSaving'] = 'Fehler beim Speichern';
-$lang['js']['errorUnsupportedFormat'] = 'Nicht unterstützt!';
-$lang['js']['saving'] = 'Speichern';
-$lang['js']['PMMenuItem'] = 'Diagramm';
-$lang['js']['selectSource'] = 'Datei wählen';
-$lang['js']['mediaSource'] = 'Mediendatei';
-$lang['js']['alignment'] = 'Ausrichtung';
-$lang['js']['left'] = 'links';
-$lang['js']['center'] = 'zentriert';
-$lang['js']['right'] = 'rechts';
-$lang['js']['title'] = 'Titel';
+$lang['js']['editButton']      = 'Diagramm editieren';
+$lang['js']['toolbarButton']   = 'Diagramm erstellen oder bearbeiten';
+$lang['js']['errorInvalidId']  = 'Name ist leer oder enthält ungültige Zeichen!';
+$lang['js']['errorSaving']     = 'Fehler beim Speichern';
+$lang['js']['errorLoading']    = 'Laden fehlgeschlagen';
+$lang['js']['errorUnsupportedFormat'] = 'Nicht unterstütztes Dateiformat!';
+$lang['js']['saving']          = 'Speichern';
+$lang['js']['PMMenuItem-mediafile'] = 'Diagramm verlinken';
+$lang['js']['PMMenuItem-embed'] = 'Diagramm einbetten';
+$lang['js']['selectSource']    = 'Datei wählen';
+$lang['js']['mediaSource']     = 'Mediendatei';
+$lang['js']['alignment']       = 'Ausrichtung';
+$lang['js']['left']            = 'links';
+$lang['js']['center']          = 'zentriert';
+$lang['js']['right']           = 'rechts';
+$lang['js']['title']           = 'Titel';
+$lang['js']['formtitle']       = 'Diagrammeigenschaften';
+$lang['js']['mediafileIsNotDiagram'] = 'Die ausgewählte Mediendatei ist kein Diagramm';
