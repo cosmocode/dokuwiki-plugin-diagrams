3c60dc334d8875cdcefd043bfc77d5c8f9c295b6
Anna Dabrowska <dabrowska@cosmocode.de>
2023-08-07 14:41:41 +0200

    Button icons with inline SVG instead of img

--- script/DiagramsFunctions.js
+++ script/DiagramsFunctions.js
@@ -53,12 +53,23 @@ class DiagramsFunctions {
      * Icon HTML
      *
      * @param {String} button
-     * @returns {HTMLImageElement}
+     * @returns {HTMLSpanElement}
      */
     static getButtonIcon(button) {
-        const icon = document.createElement('img');
-        icon.src = DOKU_BASE + `lib/plugins/diagrams/img/${button}.svg`;
+        const icon = document.createElement('span');
         icon.className = `icon-${button}`;
+
+        switch (button) {
+            case "open":
+                icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg>';
+                break;
+            case "edit":
+                icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
+                break;
+            case "download":
+                icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" /></svg>';
+                break;
+        }
 
         return icon;
     }
