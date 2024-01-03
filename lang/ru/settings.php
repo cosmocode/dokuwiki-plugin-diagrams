a7fc78f309ca1819c2cbf4e56e09ef34dca4b6e0
Aleksandr Selivanov <alexgearbox@yandex.ru>
2024-01-03 18:50:39 +0100

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['mode']                  = 'Как следует хранить диаграммы?';
+$lang['pngcache']              = 'Кэшировать диаграммы в формате PNG? (рекомендуется при использовании плагина dw2pdf)';
+$lang['mode_o_1']              = 'медиафайлом';
+$lang['mode_o_2']              = 'встраиванием в страницу';
+$lang['mode_o_3']              = 'разрешить оба способа';
