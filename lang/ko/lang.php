93034d18d33fe69f1ea9f7648488eb14ac742668
SoiBKK <info@soibangkok.online>
2025-05-24 06:10:50 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author SoiBKK <info@soibangkok.online>
+ */
+$lang['js']['createButton']    = '만들기';
+$lang['js']['createLink']      = '다이어그램 만들기';
+$lang['js']['editButton']      = '다이어그램 편집';
+$lang['js']['downloadSVGButton'] = '다이어그램을 SVG로 다운로드';
+$lang['js']['downloadPNGButton'] = '다이어그램을 PNG로 다운로드';
+$lang['js']['openButton']      = '다이어그램 열기';
+$lang['js']['editButtonShort'] = '편집';
+$lang['js']['downloadSVGButtonShort'] = 'SVG 다운로드';
+$lang['js']['downloadPNGButtonShort'] = 'PNG 다운로드';
+$lang['js']['openButtonShort'] = '열기';
+$lang['js']['toolbarButton']   = '다이어그램을 새로 만들거나 편집하기';
+$lang['js']['errorSaving']     = '저장 실패';
+$lang['js']['errorLoading']    = '불러오기 실패';
+$lang['js']['selectSource']    = '파일 선택';
+$lang['js']['left']            = '왼쪽';
+$lang['js']['center']          = '가운데';
+$lang['js']['right']           = '오른쪽';
+$lang['js']['title']           = '제목';
+$lang['js']['mediafileIsNotDiagram'] = '다이어그램이 아닌 미디어 파일이 선택 됨';
---
+++ lang/ko/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author SoiBKK <info@soibangkok.online>
+ */
+$lang['mode_o_1']              = '미디어 파일';
