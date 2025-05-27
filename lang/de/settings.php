7bd239d5be0efb3f7618e3ba9b5812f827e1c185
Andreas Heil <aeheil@gmail.com>
2025-05-27 22:30:06 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,10 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Andreas Heil <aeheil@gmail.com>
  * @author Andreas Gohr <gohr@example.com>
  */
 $lang['service_url']           = 'URL um den diagrams.net Editor aufzurufen';
 $lang['mode']                  = 'Wie sollen Diagramme gespeichert werden?';
+$lang['pngcache']              = 'Sollen Diagramme auch als PNG-Datei zwischengespeichert werden? (empfohlen, wenn das Plugin dw2pdf genutzt wird)';
 $lang['mode_o_1']              = 'Mediendateien';
 $lang['mode_o_2']              = 'In Seite eingebettet';
 $lang['mode_o_3']              = 'Beides erlauben';
